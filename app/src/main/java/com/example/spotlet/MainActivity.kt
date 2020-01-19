b45f378ac502da9a3d4dcd2d314c80f39e671d49
Nicole Pav <>
2020-01-18 21:36:06 -0800

    Added listeners for main login/register buttons that open the Login and Register activities respectively Fixed the default activity error so that the project runs Imported firebase authentication and connected to firebase

--- app/src/main/java/com/example/spotlet/MainActivity.kt
+++ app/src/main/java/com/example/spotlet/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.spotlet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_mainlogin = findViewById<Button>(R.id.button_mainlogin)
+        val btn_mainregister = findViewById<Button>(R.id.button_mainregister)
+
+        btn_mainlogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        btn_mainregister.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
+
+
+
+
+
--- app/src/main/java/com/example/spotlet/Register.kt
+++ app/src/main/java/com/example/spotlet/Register.kt
@@ -2,11 +2,21 @@ package com.example.spotlet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+
+    //Firebase references
+    private var mAuth: FirebaseAuth? = null
+
+//    private FirebaseAuth mAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
     }
+
+
 }
