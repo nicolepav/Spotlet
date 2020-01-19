1db35f26fda4bac163c549dc2b67f9dc2e6dd009
Nicole Pav <>
2020-01-19 01:01:52 -0800

    Login authentication for existing users

--- app/src/main/java/com/example/spotlet/Register.kt
+++ app/src/main/java/com/example/spotlet/Register.kt
@@ -21,7 +21,7 @@ class Register : AppCompatActivity() {
 
     private var etEmail: EditText? = null
     private var etPassword: EditText? = null
-    private var btnCreateAccount: Button? = null
+    private var btnregister: Button? = null
 
     private var email: String? = null
     private var password: String? = null
@@ -33,21 +33,17 @@ class Register : AppCompatActivity() {
 
         etEmail = findViewById<View>(R.id.editText_emaillogin_register) as EditText
         etPassword = findViewById<View>(R.id.editText_createpassword) as EditText
-        btnCreateAccount = findViewById<View>(R.id.button_register) as Button
+        btnregister = findViewById<View>(R.id.button_register) as Button
 
 
         mAuth = FirebaseAuth.getInstance()
 
-        btnCreateAccount!!.setOnClickListener {
+        btnregister!!.setOnClickListener {
             createNewAccount()
             // redirect to home page
             // val intent = Intent(this, Register::class.java)
             // startActivity(intent)
-
         }
-
-
-
 
     }
 
@@ -55,8 +51,8 @@ class Register : AppCompatActivity() {
         email = etEmail?.text.toString()
         password = etPassword?.text.toString()
 
-        Log.d("DEBUG", email)
-        Log.d("DEBUG", password)
+//        Log.d("DEBUG", email)
+//        Log.d("DEBUG", password)
 
         mAuth!!
             .createUserWithEmailAndPassword(email!!, password!!).addOnCompleteListener(this) { task ->
@@ -64,7 +60,7 @@ class Register : AppCompatActivity() {
                     val userId = mAuth!!.currentUser!!.uid
 
                     Log.d("DEBUG", userId)
-                    Toast.makeText(this, "Register success. "+userId,
+                    Toast.makeText(this, "Registeration succeeded. \n"+userId,
                         Toast.LENGTH_SHORT).show()
                 } else {
                     Log.d("DEBUG", "WE FAILED")
