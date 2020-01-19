8d1c7a893daead0e3853b90ef6cd6f270afd45cc
nicolepav <48871049+nicolepav@users.noreply.github.com>
2020-01-19 10:10:00 -0800

    tester database access in login page fixed implementations files for firebase

--- app/src/main/java/com/example/spotlet/Login.kt
+++ app/src/main/java/com/example/spotlet/Login.kt
@@ -3,13 +3,15 @@ package com.example.spotlet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.
-//import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestore
+
+val db = FirebaseFirestore.getInstance()
 
 class Login : AppCompatActivity() {
 
@@ -31,11 +33,31 @@ class Login : AppCompatActivity() {
         etEmail = findViewById<View>(R.id.editText_emaillogin) as EditText
         etPassword = findViewById<View>(R.id.editText_passwordlogin) as EditText
         btnlogin = findViewById<View>(R.id.button_login) as Button
-
-
         mAuth = FirebaseAuth.getInstance()
 
-        btnlogin!!.setOnClickListener {
+        val entry = hashMapOf(
+            "lat" to 3,
+            "lng" to 5,
+            "rate_hour" to 3
+        )
+
+
+        db.collection("listings").add(entry)
+            .addOnSuccessListener { Log.d("sdf", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w("SDfsd", "Error writing document", e) }
+        db.collection("listings").document("As66X6KeMd7QyatQHpwv").get().addOnSuccessListener {
+            document ->
+                if (document != null) {
+                    Log.d("ifnotnukk", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("else", "No such document")
+                }
+            }.addOnFailureListener { exception ->
+                Log.d("failurelistener", "get failed with ", exception)
+            }
+
+
+            btnlogin!!.setOnClickListener {
             login()
             val intent = Intent(this, AltMap::class.java)
             startActivity(intent)
